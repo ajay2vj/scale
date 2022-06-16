8bcd02044141133ddae457f98a3c904c2e730d72
ajay2vj <vjajay98@gmail.com>
2022-06-16 18:34:19 +0530

    extra forms added

--- src/StepModal.js
+++ src/StepModal.js
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import {
   Typography,
-  TextField,
   Button,
   Stepper,
   Step,
@@ -10,11 +9,12 @@ import {
 import { makeStyles } from "@material-ui/core/styles";
 import {
   useForm,
-  Controller,
   FormProvider,
-  useFormContext,
 } from "react-hook-form";
 import BasicForm from "./components/basicForm";
+import ContactForm from "./components/contactForm";
+import PersonalForm from "./components/personalForm";
+import PaymentForm from "./components/paymentForm";
 
 const useStyles = makeStyles((theme) => ({
   button: {
@@ -31,163 +31,7 @@ function getSteps() {
   ];
 }
 
-const ContactForm = () => {
-  const { control } = useFormContext();
-  return (
-    <>
-      <Controller
-        control={control}
-        name="emailAddress"
-        render={({ field }) => (
-          <TextField
-            id="email"
-            label="E-mail"
-            variant="outlined"
-            placeholder="Enter Your E-mail Address"
-            fullWidth
-            margin="normal"
-            {...field}
-          />
-        )}
-      />
 
-      <Controller
-        control={control}
-        name="phoneNumber"
-        render={({ field }) => (
-          <TextField
-            id="phone-number"
-            label="Phone Number"
-            variant="outlined"
-            placeholder="Enter Your Phone Number"
-            fullWidth
-            margin="normal"
-            {...field}
-          />
-        )}
-      />
-      <Controller
-        control={control}
-        name="alternatePhone"
-        render={({ field }) => (
-          <TextField
-            id="alternate-phone"
-            label="Alternate Phone"
-            variant="outlined"
-            placeholder="Enter Your Alternate Phone"
-            fullWidth
-            margin="normal"
-            {...field}
-          />
-        )}
-      />
-    </>
-  );
-};
-const PersonalForm = () => {
-  const { control } = useFormContext();
-  return (
-    <>
-      <Controller
-        control={control}
-        name="address1"
-        render={({ field }) => (
-          <TextField
-            id="address1"
-            label="Address 1"
-            variant="outlined"
-            placeholder="Enter Your Address 1"
-            fullWidth
-            margin="normal"
-            {...field}
-          />
-        )}
-      />
-      <Controller
-        control={control}
-        name="address2"
-        render={({ field }) => (
-          <TextField
-            id="address2"
-            label="Address 2"
-            variant="outlined"
-            placeholder="Enter Your Address 2"
-            fullWidth
-            margin="normal"
-            {...field}
-          />
-        )}
-      />
-      <Controller
-        control={control}
-        name="country"
-        render={({ field }) => (
-          <TextField
-            id="country"
-            label="Country"
-            variant="outlined"
-            placeholder="Enter Your Country Name"
-            fullWidth
-            margin="normal"
-            {...field}
-          />
-        )}
-      />
-    </>
-  );
-};
-const PaymentForm = () => {
-  const { control } = useFormContext();
-  return (
-    <>
-      <Controller
-        control={control}
-        name="cardNumber"
-        render={({ field }) => (
-          <TextField
-            id="cardNumber"
-            label="Card Number"
-            variant="outlined"
-            placeholder="Enter Your Card Number"
-            fullWidth
-            margin="normal"
-            {...field}
-          />
-        )}
-      />
-      <Controller
-        control={control}
-        name="cardMonth"
-        render={({ field }) => (
-          <TextField
-            id="cardMonth"
-            label="Card Month"
-            variant="outlined"
-            placeholder="Enter Your Card Month"
-            fullWidth
-            margin="normal"
-            {...field}
-          />
-        )}
-      />
-      <Controller
-        control={control}
-        name="cardYear"
-        render={({ field }) => (
-          <TextField
-            id="cardYear"
-            label="Card Year"
-            variant="outlined"
-            placeholder="Enter Your Card Year"
-            fullWidth
-            margin="normal"
-            {...field}
-          />
-        )}
-      />
-    </>
-  );
-};
 
 function getStepContent(step) {
   switch (step) {
---
+++ src/components/contactForm/index.js
@@ -0,0 +1,62 @@
+import React from 'react'
+import {
+  Controller,
+  useFormContext,
+} from "react-hook-form";
+import {
+  TextField
+} from "@material-ui/core";
+
+export default function ContactForm() {
+  const { control } = useFormContext();
+  return (
+    <>
+      <Controller
+        control={control}
+        name="emailAddress"
+        render={({ field }) => (
+          <TextField
+            id="email"
+            label="E-mail"
+            variant="outlined"
+            placeholder="Enter Your E-mail Address"
+            fullWidth
+            margin="normal"
+            {...field}
+          />
+        )}
+      />
+
+      <Controller
+        control={control}
+        name="phoneNumber"
+        render={({ field }) => (
+          <TextField
+            id="phone-number"
+            label="Phone Number"
+            variant="outlined"
+            placeholder="Enter Your Phone Number"
+            fullWidth
+            margin="normal"
+            {...field}
+          />
+        )}
+      />
+      <Controller
+        control={control}
+        name="alternatePhone"
+        render={({ field }) => (
+          <TextField
+            id="alternate-phone"
+            label="Alternate Phone"
+            variant="outlined"
+            placeholder="Enter Your Alternate Phone"
+            fullWidth
+            margin="normal"
+            {...field}
+          />
+        )}
+      />
+    </>
+  );
+};
